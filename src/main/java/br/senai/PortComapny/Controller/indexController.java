bdd1dc49e700cb368a6b7c72ab38cc8fb60e7ef8
ANNA LAURA BERTI FIAMETI <anna_fiameti@estudante.sc.senai.br>
2022-10-18 10:03:51 -0300

    atualização

--- src/main/java/br/senai/PortComapny/Controller/indexController.java
+++ src/main/java/br/senai/PortComapny/Controller/indexController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 
-    @GetMapping("/cadastrar")
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/")
     public String cadastrar() {
         return "cadastrar";}
 
---
+++ src/main/java/br/senai/PortComapny/Model/Usuario.java
@@ -0,0 +1,71 @@
+package br.senai.PortComapny.Model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    @Size(max = 100)
+    @NotNull
+    private String nome;
+    @Size(max = 15)
+    private String username;
+    @Size(max = 30)
+    private String telefone;
+    private String email;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario:" +
+                "id:" + id +
+                "nome:" + nome +
+                "username:" + username +
+                "telefone:" + telefone +
+                "email:" + email +
+                "senha:" + senha;
+    }
+}
